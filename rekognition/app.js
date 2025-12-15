5fa7ec12c537b28e7b151c13baeedb8367039752
Manar Chafei <79658236+manar-chafei@users.noreply.github.com>
2025-12-15 16:14:05 +0100

    Change greeting message in app.jstt

--- rekognition/app.js
+++ rekognition/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
- res.send('Hello Manar!');
+ res.send('Hello examen tp!');
 });
 app.listen(port, () => {
  console.log(`Example app listening at http://localhost:${port}`);
